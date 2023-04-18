39cd25137c0ffeeb5bd3496785422b0ba1699cde
IdanZR <63792038+IdanZR@users.noreply.github.com>
2023-04-18 22:48:25 +0300

    Create gradient_descent_imp.py

---
+++ ML/Supervised Machine Learning: Regression and Classification/WEEK 1/gradient_descent_imp.py
@@ -0,0 +1,56 @@
+import math, copy
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.style.use('deeplearning.mplstyle')
+from lab_utils_uni import plt_house_x, plt_contour_wgrad, plt_divergence, plt_gradients
+
+# Load our data set
+x_train = np.array([1.0, 2.0])  # features
+y_train = np.array([300.0, 500.0])  # target value
+
+
+# Function to calculate the cost
+def compute_cost(x, y, w, b):
+    m = x.shape[0]  # sizez of the medgam
+    cost = 0
+
+    for i in range(m):
+        f_wb = w * x[i] + b
+        cost = cost + (f_wb - y[i]) ** 2
+    total_cost = 1 / (2 * m) * cost
+
+    return total_cost
+
+
+def compute_gradient(x, y, w, b):
+    """
+    Computes the gradient for linear regression
+    Args:
+      x (ndarray (m,)): Data, m examples
+      y (ndarray (m,)): target values
+      w,b (scalar)    : model parameters
+    Returns
+      dj_dw (scalar): The gradient of the cost w.r.t. the parameters w
+      dj_db (scalar): The gradient of the cost w.r.t. the parameter b
+     """
+
+    # Number of training examples
+    m = x.shape[0]
+    dj_dw = 0
+    dj_db = 0
+
+    for i in range(m):
+        f_wb = w * x[i] + b
+        dj_dw_i = (f_wb - y[i]) * x[i]
+        dj_db_i = f_wb - y[i]
+        dj_db += dj_db_i
+        dj_dw += dj_dw_i
+    dj_dw = dj_dw / m
+    dj_db = dj_db / m
+
+    return dj_dw, dj_db
+
+
+plt_gradients(x_train, y_train, compute_cost, compute_gradient)
+plt.show()
