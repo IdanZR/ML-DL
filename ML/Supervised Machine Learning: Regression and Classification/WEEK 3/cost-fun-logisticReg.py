c1df5e034adba3cff84b9f7f7b8d4495ea018313
IdanZR <63792038+IdanZR@users.noreply.github.com>
2023-04-27 21:10:34 +0300

    Create cost-fun-logisticReg.py

---
+++ ML/Supervised Machine Learning: Regression and Classification/WEEK 3/cost-fun-logisticReg.py
@@ -0,0 +1,23 @@
+def compute_cost_logistic(X, y, w, b):
+    """
+    Computes cost
+
+    Args:
+      X (ndarray (m,n)): Data, m examples with n features
+      y (ndarray (m,)) : target values
+      w (ndarray (n,)) : model parameters  
+      b (scalar)       : model parameter
+      
+    Returns:
+      cost (scalar): cost
+    """
+
+    m = X.shape[0]
+    cost = 0.0
+    for i in range(m):
+        z_i = np.dot(X[i],w) + b
+        f_wb_i = sigmoid(z_i)
+        cost +=  -y[i]*np.log(f_wb_i) - (1-y[i])*np.log(1-f_wb_i)
+             
+    cost = cost / m
+    return cost
